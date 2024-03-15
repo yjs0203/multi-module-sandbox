fc53a73b758a99b209238b86b0345a2671ae0f0f
yeah0203 <yeah0203@naver.com>
2024-03-15 10:17:17 +0900

    reset

---
+++ src/main/java/com/example/multimodulesandbox/MultiModuleSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.multimodulesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModuleSandboxApplication.class, args);
+	}
+
+}
